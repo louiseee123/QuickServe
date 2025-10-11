22b2cdd46839c8d0b88282fc69b2d13d9cb22d0f
johnlouisebergs123 <johnlouisebergs123@gmail.com>
2025-10-11 06:07:59 +0000

    fix: Load Appwrite credentials from .env file

--- client/src/lib/appwrite.ts
+++ client/src/lib/appwrite.ts
@@ -4,8 +4,8 @@ import { Client, Account, Databases, Storage } from 'appwrite';
 const client = new Client();
 
 client
-    .setEndpoint(import.meta.env.VITE_APP_APPWRITE_ENDPOINT) // Your Appwrite Endpoint
-    .setProject(import.meta.env.VITE_APP_APPWRITE_PROJECT_ID); // Your project ID
+    .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
+    .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
 
 export const account = new Account(client);
 export const databases = new Databases(client);
--- server/appwrite.ts
+++ server/appwrite.ts
@@ -1,16 +1,17 @@
 
-import { Client, Account, Databases, Storage } from 'node-appwrite';
+import { Client, Account, Databases, Storage, Users } from 'node-appwrite';
 import dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config({ path: '../.env' });
 
 const client = new Client();
 
 client
-    .setEndpoint(process.env.APPWRITE_ENDPOINT as string) // Your Appwrite Endpoint
-    .setProject(process.env.APPWRITE_PROJECT_ID as string) // Your project ID
-    .setKey(process.env.APPWRITE_API_KEY as string); // Your secret API key
+    .setEndpoint(process.env.APPWRITE_ENDPOINT as string)
+    .setProject(process.env.APPWRITE_PROJECT_ID as string)
+    .setKey(process.env.APPWRITE_API_KEY as string);
 
 export const account = new Account(client);
 export const databases = new Databases(client);
 export const storage = new Storage(client);
+export const users = new Users(client);
