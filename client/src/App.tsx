19a17764570e35008174eae9f5d06ac1711a6083
johnlouisebergs123 <johnlouisebergs123@gmail.com>
2025-10-11 13:53:35 +0000

    fix(app): add loading screen to prevent auth race condition

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,19 @@
 import { Toaster } from './components/ui/toaster';
 import Layout from './components/layout';
 import Routes from './Routes';
+import useAuth from './hooks/use-auth';
 
 function App() {
+  const { isLoading } = useAuth();
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center h-screen bg-blue-900 text-white">
+        <p className="text-lg">Loading QuickServe...</p>
+      </div>
+    );
+  }
+
   return (
     <>
       <Layout>
