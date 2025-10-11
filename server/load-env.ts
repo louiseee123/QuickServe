7d9f116c0677fb13aabe6b2bbde426ae930d2a21
johnlouisebergs123 <johnlouisebergs123@gmail.com>
2025-10-11 09:36:29 +0000

    fix: Correct server-side Appwrite initialization

--- server/load-env.ts
+++ server/load-env.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import path from 'path';
 
 export function loadEnv() {
-    const envPath = path.resolve(process.cwd(), '.env');
+    const envFile = process.env.NODE_ENV === 'production' ? '.env' : 'dev.env';
+    const envPath = path.resolve(process.cwd(), envFile);
     dotenv.config({ path: envPath });
 }
