e9fdc918da5a1b1aec949ec5490fcd8b9ae81fef
johnlouisebergs123 <johnlouisebergs123@gmail.com>
2025-10-12 08:54:10 +0000

    fix: Remove syntax error in request.tsx

--- client/src/pages/request.tsx
+++ client/src/pages/request.tsx
@@ -1,4 +1,4 @@
-'''
+
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
@@ -293,4 +293,3 @@ export default function Request() {
     </div>
   );
 }
-''
